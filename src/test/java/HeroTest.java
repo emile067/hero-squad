06944c6d375df99d260982d6a00023d5cc130d99
emile kamana <emileizere@gmail.com>
2020-11-29 18:59:18 +0200

    new hero has all attributes test complete

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -9,4 +9,15 @@ public class HeroTest {
         Hero newHero = new Hero("emile","18", "Coding", "Sleeping:)");
         assertTrue(newHero instanceof Hero);
     }
+
+    @Test
+    public void newHeroHasAllAttributes(){
+        Hero newHero = new Hero("emile","18", "Coding", "Sleeping:)");
+        assertEquals("emile",newHero.getHeroName());
+        assertEquals("18",newHero.getHeroAge());
+        assertEquals("Coding",newHero.getHeroPower());
+        assertEquals("Sleeping:)",newHero.getHeroWeakness());
+    }
+
+
 }
