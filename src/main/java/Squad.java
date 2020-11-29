fe28128c4a9f4805b2e38e547780aa69708ab43d
emile kamana <emileizere@gmail.com>
2020-11-29 19:41:25 +0200

    create the squad class with attributes

---
+++ src/main/java/Squad.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Squad {
+    private int maxSize;
+    private String squadName;
+    private String squadCause;
+    private static List<Squad> squadsList = new ArrayList<Squad>();
+}
