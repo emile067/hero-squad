1232559bb62d056d6a99e6dec85ee176df87d5a3
emile kamana <emileizere@gmail.com>
2020-11-29 23:18:40 +0200

    new squad contains all attributes

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -8,4 +8,15 @@ public class SquadTest {
         Squad newSquad = new Squad(4,"Marines", "Coding to another level");
         assertTrue(newSquad instanceof Squad);
     }
+
+    @Test
+    public void newSquadHasAllAttributes(){
+        Squad newSquad = new Squad(4,"Marines", "Coding to another level");
+        Hero newHero = new Hero("emile","18", "Coding", "Sleeping:)");
+        newSquad.getSquadHeroes().add(newHero);
+        assertEquals(4,newSquad.getMaxSize());
+        assertEquals("Marines",newSquad.getSquadName());
+        assertEquals("Coding to another level",newSquad.getSquadCause());
+        assertTrue(newSquad.getSquadHeroes().contains(newHero));
+    }
 }
