67fb4ce96fc37473b8aaaa8d2ad7406270babffe
emile kamana <emileizere@gmail.com>
2020-11-29 18:45:29 +0200

    new hero gets created test complete

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -1,5 +1,12 @@
+import org.junit.Test;
+import org.junit.*;
+
 import static org.junit.Assert.*;
 
 public class HeroTest {
-
+    @Test
+    public void newHeroIsCreated_true(){
+        Hero newHero = new Hero("emile","18", "Coding", "Sleeping:)");
+        assertTrue(newHero instanceof Hero);
+    }
 }
