acb5fd844beddd239de7506e80d6a861f99a1a74
emile kamana <emileizere@gmail.com>
2020-11-29 23:06:36 +0200

    creation of squad test successful

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -1,5 +1,11 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SquadTest {
-
+    @Test
+    public void newHeroIsCreated_true(){
+        Squad newSquad = new Squad(4,"Marines", "Coding to another level");
+        assertTrue(newSquad instanceof Squad);
+    }
 }
