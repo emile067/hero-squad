a78b9b5948d801fda564cb57251c3b6da5b9442a
emile kamana <emileizere@gmail.com>
2020-11-30 22:22:52 +0200

    add initial layout of homepage and route to it

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import spark.ModelAndView;
+
 public class App {
     public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/",(req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Hero> heroes = Hero.getHeroesList();
+            model.put("heroes",heroes);
+            return new ModelAndView(model,"index.hbs");
+        },new HandlebarsTemplateEngine());
 
     }
+
 }
