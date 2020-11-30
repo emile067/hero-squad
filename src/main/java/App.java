195a973d6cd64c45d58aa285395a309cc0d197a2
emile kamana <emileizere@gmail.com>
2020-11-30 22:25:29 +0200

    add routes to access the form and add hero to list

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,6 +10,23 @@ import spark.ModelAndView;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+        get("/hero/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Squad> squads= Squad.getSquadsList();
+            model.put("squads",squads);
+            return new ModelAndView(model, "hero-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/hero", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            String heroName = req.queryParams("heroName");
+            String age = req.queryParams("heroAge");
+            String power = req.queryParams("heroPower");
+            String weakness = req.queryParams("heroWeakness");
+            Hero newHero = new Hero(heroName,age,power,weakness);
+            res.redirect("/");
+            return null;
+        }, new HandlebarsTemplateEngine());
 
         get("/",(req, res) -> {
             Map<String, Object> model = new HashMap<>();
