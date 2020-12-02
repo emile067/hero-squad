6ff3553070934b568634a9d23f9ad57f66d90b54
emile kamana <emileizere@gmail.com>
2020-12-02 13:25:28 +0200

    first deployment attempt

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,7 +8,15 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import spark.ModelAndView;
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
         get("/hero/new", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
